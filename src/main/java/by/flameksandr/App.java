f1e4fbf0d16fae2c4bf8c16ccdd802bfd89aad85
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-11 14:38:21 +0300

    insert values with the help of insert query

--- src/main/java/by/flameksandr/App.java
+++ src/main/java/by/flameksandr/App.java
@@ -7,25 +7,15 @@ public class App {
         String url = "jdbc:mysql://localhost:3306/student?useSSL=false";
         String userName = "root";
         String password = "password";
-        String query = "select * from nepal";
+        String query = "insert into nepal values (107, 'hiroky', 3450990)";
 
         Class.forName("com.mysql.cj.jdbc.Driver");
 
         Connection connection = DriverManager.getConnection(url, userName, password);
         Statement st = connection.createStatement();
-        ResultSet resultSet = st.executeQuery(query);
-        resultSet.next();
+        int count = st.executeUpdate(query);
+        System.out.println(count + " row/s affected");
 
-        String name;
-
-        while (resultSet.next()) {
-
-            name = resultSet.getInt(1) + " : " +
-                    resultSet.getString(2) + " : " +
-                    resultSet.getInt(3);
-
-            System.out.println(name);
-        }
         st.close();
         connection.close();
     }
