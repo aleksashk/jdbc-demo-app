dc78f85e1831768b5f70798821ac47e048b24237
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-11 14:54:54 +0300

    inserting values when values are coming dynamically with help PreparedStatement instead statement

--- src/main/java/by/flameksandr/App.java
+++ src/main/java/by/flameksandr/App.java
@@ -1,22 +1,50 @@
 package by.flameksandr;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         String url = "jdbc:mysql://localhost:3306/student?useSSL=false";
         String userName = "root";
         String password = "password";
-        String query = "insert into nepal values (107, 'hiroky', 3450990)";
+        String query = "insert into nepal values (?, ?, ?)";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Employee id: ");
+        int uid = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Enter the employee_name: ");
+        String uname = scanner.nextLine();
+        System.out.println("Enter the salary: ");
+
+        while (!scanner.hasNextDouble()) {
+            System.out.println("Invalid input. Please enter a valid number for salary.");
+            scanner.next();
+        }
+
+        double usalary = scanner.nextDouble();
 
         Class.forName("com.mysql.cj.jdbc.Driver");
 
         Connection connection = DriverManager.getConnection(url, userName, password);
-        Statement st = connection.createStatement();
-        int count = st.executeUpdate(query);
-        System.out.println(count + " row/s affected");
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setInt(1, uid);
+        preparedStatement.setString(2, uname);
+        preparedStatement.setDouble(3, usalary);
 
-        st.close();
+        int k = preparedStatement.executeUpdate();
+        if (k != 0) {
+            System.out.println("Data is added successfully");
+        } else {
+            System.out.println("Something is wrong");
+        }
+        connection.setAutoCommit(true);
+        preparedStatement.close();
         connection.close();
     }
 }
